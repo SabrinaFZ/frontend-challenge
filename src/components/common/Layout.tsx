94b939181f671a289cca348b304d658177d44824
SabrinaFZ <sabrina.fer.zam@gmail.com>
2025-04-17 18:22:49 +0200

    refactor: change style of layout

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,13 +1,13 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="bg-volkswagen-blue text-white p-4">
+      <header className="p-4">
         <h1 className="text-2xl">Volkswagen - Car Inventory</h1>
       </header>
-      <div className="container mx-auto px-4 py-4">
+      <div className="container mx-auto p-4">
         <main className="flex-grow p-4">{children}</main>
       </div>
-      <footer className="bg-volkswagen-blue text-white p-4 text-center">
+      <footer className="p-4 text-center text-xs">
         &copy; {new Date().getFullYear()} Car Inventory
       </footer>
     </div>
