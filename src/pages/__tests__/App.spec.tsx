2a24eee0d42853bd760bda59c63bd298a2673878
Sabrina <sabrina.fer.zam@gmail.com>
2025-04-17 19:41:57 +0200

    feat: create data table (#1)
    * feat: create data table
    
    * refactor: change style of layout
    
    * feat: configure unit test for useApp
    
    * feat: create unit test for App
    
    * refactor: modify path in useApp
    
    * feat: create unit test for formatPrice
    
    * refactor: change classnames for layout

---
+++ src/pages/__tests__/App.spec.tsx
@@ -0,0 +1,88 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { vi, describe, expect, it, afterEach, beforeEach } from "vitest";
+import App from "../App";
+import * as useAppModule from "../useApp";
+import { Car } from "@/types/car";
+
+vi.mock("../useApp.tsx");
+
+describe("App", () => {
+  beforeEach(() => {
+    vi.spyOn(useAppModule, "useApp").mockReturnValue({
+      data: [],
+      loading: true,
+      error: null,
+    });
+  });
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it("should display loading state", () => {
+    render(<App />);
+
+    expect(screen.getByText("Loading...")).toBeInTheDocument();
+  });
+
+  it("should display error state", () => {
+    vi.spyOn(useAppModule, "useApp").mockReturnValue({
+      data: [],
+      loading: false,
+      error: new Error("Failed to fetch data"),
+    });
+
+    render(<App />);
+
+    expect(screen.getByText("Error: Failed to fetch data")).toBeInTheDocument();
+  });
+
+  it("should display no data message", () => {
+    vi.spyOn(useAppModule, "useApp").mockReturnValue({
+      data: [],
+      loading: false,
+      error: null,
+    });
+
+    render(<App />);
+
+    expect(screen.getByText("No data available")).toBeInTheDocument();
+  });
+
+  it("should render the table with data", async () => {
+    const mockData = [
+      {
+        id: 1,
+        model: "Golf",
+        year: 2020,
+        price: 20000,
+        engine: "1.5L",
+        transmission: "Manual",
+      },
+    ] as unknown as Car[];
+
+    vi.spyOn(useAppModule, "useApp").mockReturnValue({
+      data: mockData,
+      loading: false,
+      error: null,
+    });
+
+    render(<App />);
+
+    // Check if table headers are rendered
+    expect(screen.getByText("Id")).toBeInTheDocument();
+    expect(screen.getByText("Model")).toBeInTheDocument();
+    expect(screen.getByText("Year")).toBeInTheDocument();
+    expect(screen.getByText("Price")).toBeInTheDocument();
+    expect(screen.getByText("Engine")).toBeInTheDocument();
+    expect(screen.getByText("Transmision")).toBeInTheDocument();
+
+    // Check if table data is rendered
+    await waitFor(() => {
+      expect(screen.getByText("Golf")).toBeInTheDocument();
+      expect(screen.getByText("2020")).toBeInTheDocument();
+      expect(screen.getByText("20.000,00 €")).toBeInTheDocument();
+      expect(screen.getByText("1.5L")).toBeInTheDocument();
+      expect(screen.getByText("Manual")).toBeInTheDocument();
+    });
+  });
+});
