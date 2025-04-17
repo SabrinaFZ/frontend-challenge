71c3bf4302fa6e3dbfb19e9ad9dfdd2aa132d3d2
SabrinaFZ <sabrina.fer.zam@gmail.com>
2025-04-18 01:22:33 +0200

    refactor: remove console.log

--- src/context/useAppState.tsx
+++ src/context/useAppState.tsx
@@ -5,12 +5,10 @@ export const useAppState = () => {
   const [data, setData] = useState<Car[]>([]);
 
   const set = useCallback((newData: Car[]) => {
-    console.log("Setting data:", newData);
     setData([...newData]);
   }, []);
 
   const add = useCallback((item: Car) => {
-    console.log("Adding item:", item);
     setData((prevData) => [...prevData, item]);
   }, []);
 
