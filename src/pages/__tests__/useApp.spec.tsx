4681328dd1686d891b3a7a516b3b21045ce6eade
SabrinaFZ <sabrina.fer.zam@gmail.com>
2025-04-17 19:28:22 +0200

    refactor: modify path in useApp

--- src/pages/__tests__/useApp.spec.tsx
+++ src/pages/__tests__/useApp.spec.tsx
@@ -1,6 +1,6 @@
 import { renderHook, waitFor } from "@testing-library/react";
 import { describe, it, expect, afterEach, vi } from "vitest";
-import { useApp } from "../../pages/useApp";
+import { useApp } from "../useApp";
 
 describe("useApp", () => {
   afterEach(() => {
