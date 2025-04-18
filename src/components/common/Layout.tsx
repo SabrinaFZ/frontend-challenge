b6a43ef16f40e064b60b015edd64fa1b82a66aa0
SabrinaFZ <sabrina.fer.zam@gmail.com>
2025-04-18 22:58:00 +0200

    refactor: add a max width for layout

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
-    <div className="flex flex-col min-h-screen p-4">
+    <div className="flex flex-col min-h-screen p-4 max-w-screen-2xl mx-auto">
       <header className="mb-4">
         <h1 className="text-2xl">Volkswagen - Car Inventory</h1>
       </header>
