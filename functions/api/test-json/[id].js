730d69b99fbc1c3ce6ce4d5b7fe3c7668485d7c6
SabrinaFZ <sabrina.fer.zam@gmail.com>
2025-04-19 14:14:54 +0200

    test: remove extra bracket

---
+++ functions/api/test-json/[id].js
@@ -0,0 +1,45 @@
+import vehicles from "./data.js";
+
+export function onRequestGet(context) {
+  const id = context.params.id;
+
+  if (!id) {
+    return new Response("Not found", { status: 404 });
+  }
+
+  const vehicle = vehicles.find((vehicle) => vehicle.id === id);
+
+  if (!vehicle) {
+    return new Response("Not found", { status: 404 });
+  }
+
+  return Response.json(vehicle);
+}
+/* 
+export function onRequestPut(request) {
+  const { id, model, year, price, engine, transmission } = request.json();
+  const vehicleIndex = vehicles.findIndex((vehicle) => vehicle.id === id);
+  if (vehicleIndex === -1) {
+    return Response.json({ error: "Vehicle not found" }, { status: 404 });
+  }
+  vehicles[vehicleIndex] = {
+    id,
+    model,
+    year,
+    price,
+    engine,
+    transmission,
+  };
+  return Response.json(vehicles[vehicleIndex]);
+}
+
+export function onRequestDelete(request) {
+  const { id } = request.json();
+  const vehicleIndex = vehicles.findIndex((vehicle) => vehicle.id === id);
+  if (vehicleIndex === -1) {
+    return Response.json({ error: "Vehicle not found" }, { status: 404 });
+  }
+  const deletedVehicle = vehicles.splice(vehicleIndex, 1);
+  return Response.json(deletedVehicle[0]);
+}
+ */
